09c73f4057de24c5b9f220829ea40155b3e0fbed
Mikko Kivelä <mikko@smartly.io>
2017-07-28 17:11:46 +0300

    Add log out and help to quest listing nav bar

--- src/quest-list.js
+++ src/quest-list.js
@@ -9,6 +9,11 @@ import {
 import {refetch} from './lib/api';
 import {compose, mapProps} from 'recompose';
 import {withRouter} from 'react-router';
+import Popover, {PopoverAnimationVertical} from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import DropdownIcon from 'react-icons/lib/md/arrow-drop-down';
+import {logOut} from './lib/auth';
 
 const items = Array(10).fill(null).map((_, i) => ({
   id: i,
@@ -17,15 +22,38 @@ const items = Array(10).fill(null).map((_, i) => ({
 }));
 
 class QuestList extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    };
+  }
   render() {
     const {quests} = this.props;
 
     return (
       <Page>
         <NavBar
-          title="Quests Board"
+          title={<div><span style={{verticalAlign: 'middle'}} ref={this.setTitleElement} onClick={this.toggleMenu}>Quests Board</span> <DropdownIcon size={16}/></div>}
           onScan={this.navigateToCodes}
           onStats={this.navigateToStatsPage}/>
+        <Popover
+          open={this.state.menuOpen}
+          anchorEl={this.title}
+          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          onRequestClose={this.closeMenu}
+          animation={PopoverAnimationVertical}
+        >
+          <Menu>
+            <MenuItem
+              primaryText="Help"
+              onTouchTap={this.navigateToHelp}/>
+            <MenuItem
+              primaryText="Sign out"
+              onTouchTap={this.logOut}/>
+          </Menu>
+        </Popover>
         <PageBody>
           <Grid>
             {quests.map(this.renderQuestCard)}
@@ -33,6 +61,28 @@ class QuestList extends React.PureComponent {
         </PageBody>
       </Page>
     );
+  }
+
+  navigateToHelp = () => {
+    this.props.history.push('/help');
+  }
+
+  logOut = () => {
+    logOut().then(() => {
+      this.props.history.replace('/login');
+    });
+  }
+
+  toggleMenu = () => {
+    this.setState({menuOpen: !this.state.menuOpen})
+  }
+
+  closeMenu = () => {
+    this.setState({menuOpen: false});
+  }
+
+  setTitleElement = (element) => {
+    this.title = element;
   }
 
   navigateToStatsPage = () => {
